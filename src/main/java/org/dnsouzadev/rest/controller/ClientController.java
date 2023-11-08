520fb159fe17c257589a708be9944dd664ddd7cf
Daniel Souza <workdndsza@gmail.com>
2023-11-08 10:25:53 -0300

    criando o controller de clientes

--- src/main/java/org/dnsouzadev/rest/controller/ClientController.java
+++ src/main/java/org/dnsouzadev/rest/controller/ClientController.java
@@ -1,4 +1,18 @@
 package org.dnsouzadev.rest.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/api/clientes")
 public class ClientController {
+
+    @RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
+    @ResponseBody
+    public String helloCliente( @PathVariable("nome") String nomeCliente ) {
+        return String.format("Hello %s ", nomeCliente);
+    }
 }
